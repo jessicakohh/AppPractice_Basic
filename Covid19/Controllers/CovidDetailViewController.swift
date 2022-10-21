0ef938e1d0d54d242d211f5128d6e7c3fc3b26ce
jessicakohh <juko95@me.com>
2022-10-21 19:50:11 +0900

    Default UI Configuration

---
+++ Covid19/Controllers/CovidDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CovidDetailViewController.swift
+//  Covid19
+//
+//  Created by juyeong koh on 2022/10/21.
+//
+
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+    
+    @IBOutlet weak var newCaseCell: UITableViewCell!
+    @IBOutlet weak var totalCaseCell: UITableViewCell!
+    @IBOutlet weak var recoveredCell: UITableViewCell!
+    @IBOutlet weak var deathCell: UITableViewCell!
+    @IBOutlet weak var percentageCell: UITableViewCell!
+    @IBOutlet weak var overseasInflowCell: UITableViewCell!
+    @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+  
+
+}
--- Covid19/Controllers/ViewController.swift
+++ Covid19/Controllers/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalCaseLabel: UIStackView!
+    @IBOutlet weak var newCaseLabel: UIStackView!
+    @IBOutlet weak var pieChartView: PieChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
